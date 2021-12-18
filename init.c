22259e3a580b6aca1f8bceaa07ac6e57df36caf0
김동휘 <v2008v@daum.net>
2021-12-18 21:12:15 +0900

    done

--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: donghwik <donghwik@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 15:18:02 by donghwik          #+#    #+#             */
-/*   Updated: 2021/12/18 21:09:16 by donghwik         ###   ########.fr       */
+/*   Updated: 2021/12/18 21:11:56 by donghwik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,6 @@ int	mutex_init(t_info *info)
 
 int	destroy_all_mutex(t_info *info, int index)
 {
-	pthread_mutex_destroy(&(info->print));
 	while (index >= 0)
 		pthread_mutex_destroy(&(info->forks[--index]));
 	return (-1);
@@ -78,6 +77,9 @@ int	init(t_info *info)
 	if (mutex_init(info) == -1)
 		return (-1);
 	if (philo_init(info) == -1)
+	{
+		pthread_mutex_destroy(&(info->print));
 		return (destroy_all_mutex(info, info->nop));
+	}
 	return (0);
 }
